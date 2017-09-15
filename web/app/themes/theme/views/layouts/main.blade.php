a1c266d6fd8b2de83526b0b8a93203e0d73ee97a
Zorca <vs@zorca.org>
2017-09-15 20:06:59 +0300

    Styles updated

--- web/app/themes/theme/views/layouts/main.blade.php
+++ web/app/themes/theme/views/layouts/main.blade.php
@@ -3,12 +3,13 @@
   @include('partials.head')
   <body @php(body_class())>
     @php(do_action('get_header'))
-    @include('partials.header')
-    <div class="l-main">
+    <div class="sidebar">
+      @include('partials/sidebar')
+    </div>
+    <div class="main">
       @yield('content')
     </div>
     @php(do_action('get_footer'))
-    @include('partials.footer')
     @php(wp_footer())
   </body>
 </html>
